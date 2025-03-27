c10214febf69f57f0c0a4028b1a80e2dd586264c
Akshat Yuvan <akshatyuvan2004@gmail.com>
2025-03-28 01:32:51 +0530

    Read Data Set

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -2,4 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+data=pd.read_csv('/Users/akshatyuvan/github-classroom/Finova-MIT/intro-to-ml-akshatyuvan/HousingData.csv')
+
 
